3486a8453418b928c71ddea6de6ad94ee3124830
Leo Auri <code@leoauri.com>
2021-10-14 15:42:32 +0200

    Use blocking sleep to fix processor usage

--- switch2osc.py
+++ switch2osc.py
@@ -2,7 +2,7 @@ from pyjoycon import JoyCon, get_L_id, get_R_id
 
 from pythonosc import udp_client
 
-from time import monotonic as monotime
+import time 
 
 import pprint
 
@@ -22,7 +22,6 @@ except ValueError:
 
 osc = udp_client.SimpleUDPClient("127.0.0.1", 7331)
 
-done = None
 wait_time = 0.01
 if wait_time is not 0:
     print(f'Running at {1/wait_time} Hz, refresh {wait_time*1000} ms')
@@ -33,15 +32,14 @@ if joycon_l is not None:
     pp.pprint(joycon_l.get_status())
 
 while True:
-    if done is None or monotime() > done + wait_time:
-        done = monotime()
-        if joycon_l is not None:
-            jc_l = joycon_l.get_status()
-            gyro = jc_l['gyro']
-            # print(gyro)
-            osc.send_message("/gyro_x", gyro['x'])
-            osc.send_message("/gyro_y", gyro['y'])
-            osc.send_message("/gyro_z", gyro['z'])
+    if joycon_l is not None:
+        jc_l = joycon_l.get_status()
+        gyro = jc_l['gyro']
+        osc.send_message("/gyro_x", gyro['x'])
+        osc.send_message("/gyro_y", gyro['y'])
+        osc.send_message("/gyro_z", gyro['z'])
 
-            button_l_l = jc_l['buttons']['left']['l']
-            osc.send_message('/button_l_l', button_l_l)
+        button_l_l = jc_l['buttons']['left']['l']
+        osc.send_message('/button_l_l', button_l_l)
+
+    time.sleep(wait_time)
