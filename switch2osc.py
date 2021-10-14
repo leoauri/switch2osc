8df55615c1e37f24950b9fafb3fc9944b4fec709
Leo Auri <code@leoauri.com>
2021-10-14 16:13:53 +0200

    Send all controls

--- switch2osc.py
+++ switch2osc.py
@@ -6,6 +6,21 @@ import time
 
 import pprint
 
+sent_on = []
+
+
+def send_dict(addr, input):
+    if isinstance(input, dict):
+        for key, value in input.items():
+            send_dict(addr + '/' + key, value)
+    else:
+        osc.send_message(addr, input)
+        if addr not in sent_on:
+            print(f'Sent on {addr}')
+            sent_on.append(addr)
+
+
+
 joycon_id_l = get_L_id()
 joycon_id_r = get_R_id()
 
@@ -15,31 +30,34 @@ except ValueError:
     print('Could not connect left joycon')
     joycon_l = None
 
-# try:
-#     joycon_r = JoyCon(*joycon_id_r)
-# except ValueError:
-#     print('Could not connect right joycon')
+try:
+    joycon_r = JoyCon(*joycon_id_r)
+except ValueError:
+    print('Could not connect right joycon')
+    joycon_r = None
 
 osc = udp_client.SimpleUDPClient("127.0.0.1", 7331)
 
 wait_time = 0.01
-if wait_time is not 0:
+if wait_time != 0:
     print(f'Running at {1/wait_time} Hz, refresh {wait_time*1000} ms')
 
 pp = pprint.PrettyPrinter(indent=4)
 
 if joycon_l is not None:
+    print('Left joycon connected')
     pp.pprint(joycon_l.get_status())
+
+if joycon_r is not None:
+    print('Right joycon connected')
+    pp.pprint(joycon_r.get_status())
 
 while True:
     if joycon_l is not None:
         jc_l = joycon_l.get_status()
-        gyro = jc_l['gyro']
-        osc.send_message("/gyro_x", gyro['x'])
-        osc.send_message("/gyro_y", gyro['y'])
-        osc.send_message("/gyro_z", gyro['z'])
-
-        button_l_l = jc_l['buttons']['left']['l']
-        osc.send_message('/button_l_l', button_l_l)
+        send_dict('/joycon_l', jc_l)
+    if joycon_r is not None:
+        jc_r = joycon_r.get_status()
+        send_dict('/joycon_r', jc_r)
 
     time.sleep(wait_time)
